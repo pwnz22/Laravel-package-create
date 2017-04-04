1c9c2be46344fa949d1ab86feb0dbd123ccdb1ad
Valijon <parliament888@gmail.com>
2017-04-04 08:08:47 +0800

    ModelFactory for TagModel

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Pwnz22\Taggz\Models\Tag::class, function (Faker\Generator $faker) {
+    $name = $faker->unique()->colorName;
+
+    return [
+        'name' => $name,
+        'slug' => str_slug($name)
+    ];
+});
