ca0afd5ed602e7eed34f1a1f1fd9b3116cf9ab84
Valijon <parliament888@gmail.com>
2017-04-04 08:46:59 +0800

    TaggableTrait methods

--- packages/pwnz22/taggz/src/TaggableTrait.php
+++ packages/pwnz22/taggz/src/TaggableTrait.php
@@ -3,11 +3,55 @@
 namespace Pwnz22\Taggz;
 
 use Pwnz22\Taggz\Models\Tag;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 trait TaggableTrait
 {
+    private function getWorkableTags($tags)
+    {
+        if (is_array($tags)) {
+            return $this->getTagModels($tags);
+        }
+
+        if ($tags instanceof Model) {
+            return $this->getTagModels([$tags->slug]);
+        }
+
+        return $this->filterTagsCollection($tags);
+    }
+
+    private function getTagModels(array $tags)
+    {
+        return Tag::whereIn('slug', $this->normalizeTagNames($tags))->get();
+    }
+
+    private function normalizeTagNames(array $tags)
+    {
+        return array_map(function ($tag) {
+            return str_slug($tag);
+        }, $tags);
+    }
+
+    private function filterTagsCollection(Collection $tags)
+    {
+        return $tags->filter(function ($tag) {
+            return $tag instanceof Model;
+        });
+    }
+
+    private function addTags(Collection $tags)
+    {
+        $sync = $this->tags()->syncWithoutDetaching($tags->pluck('id')->toArray());
+    }
+
     public function tags()
     {
         return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    public function tag($tags)
+    {
+        $this->addTags($this->getWorkableTags($tags));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', function () {
     $lesson = Lesson::find(1);
 
     dd($lesson->tags);
+
+    $lesson->tag(['tomato', 'lime', 'green']);
 });
