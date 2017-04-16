c2abece92da8183d764a9f78f782d20d39b21535
Valijon <parliament888@gmail.com>
2017-04-16 11:31:55 +0800

    Problem with recursive partial include

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,16 @@ Route::get('/', function () {
 
     $comments = $article->comments()->with([
         'user',
-        'replies.parent.user',
         'replies.user',
+        'replies.parent.user',
+        'replies.replies.user',
         'replies.replies.parent.user',
-        'replies.replies.user'
-    ])->get();
+        'replies.replies.replies.user',
+        'replies.replies.replies.parent.user',
+        'replies.replies.replies.replies.user',
+        'replies.replies.replies.replies.parent.user',
+        'replies.replies.replies.replies.replies.user'
+    ])->paginate(10);
 
     return view('comments.index', compact('article', 'comments'));
 
