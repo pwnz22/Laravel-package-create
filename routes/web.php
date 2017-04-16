909c67ccb7b8ff9a867966daa6fb7dfd2ba051ae
Valijon <parliament888@gmail.com>
2017-04-16 10:16:25 +0800

    Nesting and eager loading

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,17 @@ Route::get('/', function () {
 
     $article = Article::find(1);
 
-    dd($article->comments()->get());
+    $comments = $article->comments()->with([
+        'user',
+        'replies.parent.user',
+        'replies.user',
+        'replies.replies.parent.user',
+        'replies.replies.user'
+    ])->get();
+
+    return view('comments.index', compact('article', 'comments'));
 
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
